37f7f034169938006422fb7444da962c3d33e4ac
RAVLYK <R@VLYK>
2015-05-19 17:47:57 +0300

    Initial commit

---
+++ src/com/gildedrose/Item.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gildedrose;
+
+/**
+ *
+ * @author RAVLYK
+ */
+public class Item {
+    String name;
+    int quality, sellIn;
+    
+    public Item(String name, int quality,int sellIn){
+        this.name = name;
+        this.quality = quality;
+        this.sellIn = sellIn;
+    }
+    
+    
+    
+}
